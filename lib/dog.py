fbbea7572f2bf8a1a25e6dd1884f88c9b1c7f91b
Ryan Murzyn <orian3737@gmail.com>
2024-06-28 18:58:58 -0400

    hmm

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,55 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
+APPROVED_JOBS = [
+    "Admin",
+    "Customer Service",
+    "Human Resources",
+    "ITC",
+    "Production",
+    "Legal",
+    "Finance",
+    "Sales",
+    "General Management",
+    "Research & Development",
+    "Marketing",
+    "Purchasing"
 ]
 
-class Dog:
-    pass
+class Person:
+    def __init__(self, name="Unknown", job="Unemployed"):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = None
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+            self._job = None
+
+
+if __name__ == "__main__":
+    person = Person("john doe", "ITC")
+    print(person.name) 
+    print(person.job)  
+
+    person.name = "" 
+    person.job = "Astronaut"
+
+    print(person.name) 
+    print(person.job) 
--- lib/person.py
+++ lib/person.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 APPROVED_JOBS = [
     "Admin",
     "Customer Service",
@@ -16,4 +14,42 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Unknown", job="Unemployed"):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = None
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+            self._job = None
+
+
+if __name__ == "__main__":
+    person = Person("Guido", "Sales")
+    print(person.name)  
+    print(person.job)  
+
+    person.name = ""
+    person.job = "Astronaut"  
+
+    print(person.name)
+    print(person.job)  
